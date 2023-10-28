7d031c0da5a1ca869f9abf48518636bf48d2fa88
Anirudh <anirudhece2020@gmail.com>
2023-10-28 16:49:11 +0530

    env file added

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
-console.log('im in express js home');
-console.log('hello mom');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`app is running at ${port}`);
+})
