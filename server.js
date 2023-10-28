2d8f70e1a5c85a075ca9f11cd928dfdaf6993f2f
Anirudh <anirudhece2020@gmail.com>
2023-10-28 18:05:36 +0530

    response in JSON

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.get('/api/contacts', (req, res) => {
+    res.json({'message':'get all request'});
+})
+
 app.listen(port, () => {
     console.log(`app is running at ${port}`);
 })
+
+
+
+
+
+
+
