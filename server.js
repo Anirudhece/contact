6294458367fa4b00691fc9db1af577b679801602
Anirudh <anirudhece2020@gmail.com>
2023-10-28 20:00:09 +0530

    get lagakr

--- server.js
+++ server.js
@@ -4,17 +4,8 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get('/api/contacts', (req, res) => {
-    res.send('get all request');
-})
+app.use('/api/contacts',require('./routes/contactRoutes'))// middleWare
 
 app.listen(port, () => {
     console.log(`app is running at ${port}`);
 })
-
-
-
-
-
-
-
