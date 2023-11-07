8ac95a20ae6f3d4372c0cb520cbccc17693c6bd5
Anirudh <anirudhece2020@gmail.com>
2023-11-07 23:33:41 +0530

    minor

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-
 router.route('/').get((req, res) => {
     res.json({ 'message': 'get all rotes' })
 })
-
-
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
-
 const port = process.env.PORT || 5000;
-
 app.use('/api/contacts',require('./routes/contactRoutes'))// middleWare
-
 app.listen(port, () => {
     console.log(`app is running at ${port}`);
 })
