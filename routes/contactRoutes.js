ead86dc3866f3e1c6ce2cbdf2b1b8fecd416847e
Anirudh <anirudhece2020@gmail.com>
2023-11-09 23:22:25 +0530

    tried routes

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 router.route('/').get((req, res) => {
-    res.json({ 'message': 'get all rotes' })
-})
+    res.json({ 'message': 'get all routes' })
+});
+router.route('/').post((req, res) => {
+    res.json({ 'message': 'add all routes' })
+});
+router.route('/:id').get((req, res) => {
+    res.json({ 'message': `get info from: ${req.params.id}`})
+});
+router.route('/:id').put((req, res) => {
+    res.json({ 'message': `get info from: ${req.params.id}` })
+});
+router.route('/:id').delete((req, res) => {
+    res.json({ 'message': `delete info from: ${req.params.id}` })
+});
 module.exports = router;
